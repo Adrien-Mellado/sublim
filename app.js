289e0efcb819d43ba5223049189f38f754a75384
NEDRIA <adrienmellado@outllok.fr>
2024-10-17 17:33:58 +0200

    Refont total du menu

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const menuHambuerger = document.querySelector(".menu-hamburger")
-const navlinks= document.querySelector(".nav-links")
-
-menuHambuerger.addEventListener('click', ()=>{
-    navlinks.classList.toggle('mobile_menu')
-})
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+ 
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+ 
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon')
+    })
+ }
+ 
+ showMenu('nav-toggle','nav-menu')
